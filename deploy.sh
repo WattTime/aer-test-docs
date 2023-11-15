051d87d9ad68973e805584d565c3d6a92c66b771
matthew mcginn <mamcgi@gmail.com>
2023-11-15 11:05:27 -0500

    add simple deploy script
    Signed-off-by: matthew mcginn <mamcgi@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIST_ID="E30XUKCWOUDBFR"
+OUTFILE="openapi2.json"
+
+redocly join --without-x-tag-groups ~/watttime/test-docs/auth-openapi.json ~/watttime/api-maps/openapi.json ~/watttime/apiv3-forecast/openapi.json ~/watttime/apiv3-historical/openapi.json -o "${OUTFILE}"
+aws s3 cp "${OUTFILE}" "s3://test-docs.watttime.org/openapi.json"
+aws cloudfront create-invalidation --distribution-id="${DIST_ID}" --paths /openapi.json
