3f3b36ef4cbebb4147021724f630aabf285ec7c1
matthew mcginn <mamcgi@gmail.com>
2023-11-15 15:01:28 -0500

    bump deploy process
    Signed-off-by: matthew mcginn <mamcgi@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ OUTFILE="openapi.json"
 
 redocly join --without-x-tag-groups ~/watttime/test-docs/auth-openapi.json ~/watttime/api-maps/openapi.json ~/watttime/apiv3-forecast/openapi.json ~/watttime/apiv3-historical/openapi.json -o "${OUTFILE}"
 aws s3 cp "${OUTFILE}" "s3://test-docs.watttime.org/openapi.json"
-aws cloudfront create-invalidation --distribution-id="${DIST_ID}" --paths /openapi.json
+aws s3 cp "index.html" "s3://test-docs.watttime.org/index.html"
+aws cloudfront create-invalidation --distribution-id="${DIST_ID}" --paths /openapi.json /index.html
