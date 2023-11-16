7f8c00ff87fc5f74085f131e1ce7453f9f9278cf
matthew mcginn <mamcgi@gmail.com>
2023-11-16 10:52:30 -0500

    collapse auth-openapi.json
    Signed-off-by: matthew mcginn <mamcgi@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 DIST_ID="E30XUKCWOUDBFR"
 OUTFILE="openapi.json"
 
+python generate_openapi.py > auth-openapi.json
+
 redocly join --without-x-tag-groups ~/watttime/test-docs/auth-openapi.json ~/watttime/api-maps/openapi.json ~/watttime/apiv3-forecast/openapi.json ~/watttime/apiv3-historical/openapi.json -o "${OUTFILE}"
 aws s3 cp "${OUTFILE}" "s3://test-docs.watttime.org/openapi.json"
 aws s3 cp "index.html" "s3://test-docs.watttime.org/index.html"
