f6b1f191df63751f2f3f6066e461a2b67698d312
matthew mcginn <mamcgi@gmail.com>
2023-11-17 13:44:39 -0500

    drop V3ForecastSignals refs
    Signed-off-by: matthew mcginn <mamcgi@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,6 @@ OUTFILE="openapi.json"
 ./build_docs.sh
 aws s3 cp "${OUTFILE}" "${BUCKET}/openapi.json"
 aws s3 cp "index.html" "${BUCKET}/index.html"
+aws s3 cp "WattTime-logo-2023-black-1920px_wpad.png" "${BUCKET}/WattTime-logo-2023-black-1920px_wpad.png"
+aws s3 cp "favicon.ico" "${BUCKET}/favicon.ico"
 aws cloudfront create-invalidation --distribution-id="${DIST_ID}" --paths /openapi.json /index.html
